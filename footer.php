ef337c99497e034aadb5cca78c8da22206c6eae5
pankaj <pnkjmaru93@gmail.com>
2021-08-04 16:08:28 +0530

    toggle menus fix

---
+++ footer.php
@@ -0,0 +1,96 @@
+   <!-- footer section starts here -->
+   <section class="footer_section" style="background-color: #003366;margin-top: 120px;">
+        <footer class="Footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="footer_logo-box">
+                            <a href="#top">
+                                <img src="images/footer_logo.svg" alt="footer-logo" class="img-fluid">
+                            </a>
+                        </div>
+                        <p class="footer_logo-box__para text-light">
+                            It is a long established fact that a reader will
+                            be distracted by the readable content of a page
+                            when looking at its layout. The point of using
+                            Lorem Ipsum is that it has a distribution.
+                        </p>
+                    </div>
+                    <div class="col-md-3">
+                        <h3 class="footer_service_title--ylw" style="color: #fde428;">Services</h3>
+                        <div class="footer_service_title_links_box">
+                            <ul class=" p-0">
+                                <li class="mb-3">
+                                    <a href="term.html" class="text-light text-decoration-none">Term & conditition</a>
+                                </li>
+                                <li class="footer_service__list mb-3">
+                                    <a href="#" class="text-light text-decoration-none">Income Tax</a>
+                                </li>
+                                <li class="footer_service__list mb-3">
+                                    <a href="#" class="text-light text-decoration-none">GST</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <ul class="footer_service_title_links_box2">
+                            <li class="mb-3">
+                                <a href="#" class="text-light text-decoration-none">Tax Advisory</a>
+                            </li>
+                            <li class="mb-3">
+                                <a href="#" class="text-light text-decoration-none">Setting Up New Company</a>
+                            </li>
+                            <li class="mb-3">
+                                <a href="#" class="text-light text-decoration-none">Registration</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="col-md-3">
+                        <h3 class="footer_service_title--ylw" style="color: #fde428;">Contact Us</h3>
+                        <div class="footer_service_title_links_box">
+                            <ul class="p-0">
+                                <li class="footer_service__list mb-3">
+                                    <a href="#top" class="text-light">Akshya Nagar 1st Block 1st Cross,</a>
+                                </li>
+                                <li class="footer_service__list mb-3">
+                                    <a href="#" class="text-light">Rammurthy nagar,</a>
+                                </li>
+                                <li class="footer_service__list mb-3">
+                                    <a href="#" class="text-light">Jaipur-302011</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer_menu_box mt-5">
+                    <div class="row">
+                        <ul class="footer_menu_ul text-center mx-auto">
+                            <li>
+                                <a href="#" class="text-light text-decoration-none">Home</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-light text-decoration-none">About Us</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-light text-decoration-none">Our Services</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-light text-decoration-none">Our Projects</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-light text-decoration-none">Contact Us</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="row text-right text-center">
+                        <div class="col-md-12">
+                            <div class="copy_right_box mt-5">
+                                <p class="text-light">Copyrihgts <span>&#169;</span> 2021 Tru-serve Pvt Ltd. All Rights Reserved </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </section>
+    <!-- footer section ends here -->
